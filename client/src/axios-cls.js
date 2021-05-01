bf6a2343845404763a5ca85b7cae49b046cf9d89
Naman Patel <46571853+np453@users.noreply.github.com>
2021-05-01 23:48:37 +0530

    minor changes

--- client/src/axios-cls.js
+++ client/src/axios-cls.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 const instance = axios.create({
 
     //base Domain name
-    // baseUrl : "https://calces.org.in/",
-    baseURL : "http://localhost:9000/"
+    baseUrl : "https://calces.org.in/",
+    // baseURL : "http://localhost:9000/"
 
 })
 
-// export let base = "https://calces.org.in/"
-export let base = "http://localhost:9000/"
+export let base = "https://calces.org.in/"
+// export let base = "http://localhost:9000/"
 
 export default instance
